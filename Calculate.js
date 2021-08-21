ee338315e4ac654ee9dafaceade4550286c24837
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2021-08-21 14:09:19 +0600

    update js file

--- Calculate.js
+++ Calculate.js
@@ -1,55 +1,92 @@
 
-//  function replacePrice(id)
-//  {
-//      let getExtraCost =document.getElementById('id').innerText= '180';
-//      const ExtraPrice=parseInt(getExtraCost);
-//      console.log(ExtraPrice); 
-//  }
- 
-//  function sixteenGB()
-//  { 
-//     replacePrice(extraMemoryCost); 
-//  }
+
 
 
  function eightGB()
  {
-   const getExtraMemoryCost =  document.getElementById('extraMemoryCost').innerText='20'
-  const ExtraMemoryPrice=parseInt(getExtraMemoryCost);
-  console.log(ExtraMemoryPrice);
-  
+   const getExtraMemoryCost =  document.getElementById('extraMemoryCost').innerText=0;
+   totalPriceCalculate();
  }
 
  function sixteenGB()
  {
-    let getExtraMemoryCost =  document.getElementById('extraMemoryCost').innerText='180'
-    const ExtraMemoryPrice=parseInt(getExtraMemoryCost);
-    console.log(ExtraMemoryPrice);
-    let  getTotalCost =  document.getElementById('totalPrice');
-    const totalPrice = parseInt(1299+ExtraMemoryPrice);
-    const TotalCost =getTotalCost.innerText=totalPrice;
-      
+   
+    const getExtraMemoryCost =  document.getElementById('extraMemoryCost').innerText=180;
+    totalPriceCalculate();
  }
 function ssdTowFiftyGB()
 {
-   let getExtraStorageCost =  document.getElementById('extraMemoryCost').innerText='180'
-   const ExtraMemoryPrice=parseInt(getExtraMemoryCost);
-
-
+   const getExtraStorageCost =  document.getElementById('extraStorageCost').innerText=0;
+   totalPriceCalculate();
   
 }
 function ssdFiveTwelveGB()
 {
-
+   const getExtraStorageCost =  document.getElementById('extraStorageCost').innerText=100;
+   totalPriceCalculate();
 }
 function ssdOneGB()
 {
+   const getExtraStorageCost =  document.getElementById('extraStorageCost').innerText=180;
+   totalPriceCalculate();
+}
+
+function freeDelivery()
+{
+   let getDeliveryCost =  document.getElementById('deliveryCharge').innerText=0;
+   totalPriceCalculate();
+}
+function deliveryWithCost()
+{
+   let getDeliveryCost =  document.getElementById('deliveryCharge').innerText=20;
+   totalPriceCalculate();
 
 }
-// function totalPrice()
-// {
-//    let  getTotalCost =  document.getElementById('totalPrice');
-// const totalPrice = parseInt(1299+ExtraMemoryPrice);
-// const TotalCost =getTotalCost.innerText=totalPrice;
-//    console.log(TotalCost);
-// }
+
+
+//Total Price Calculated
+
+
+
+function totalPriceCalculate(){
+   
+const getExtraMemoryCost =  document.getElementById('extraMemoryCost').innerText;
+const extraMemoryPrice=parseInt(getExtraMemoryCost);
+const getExtraStorageCost =  document.getElementById('extraStorageCost').innerText;
+const ExtraStorageCost=parseInt(getExtraStorageCost);
+const getDeliveryCost =  document.getElementById('deliveryCharge').innerText;
+const DeliveryCost=parseInt(getDeliveryCost);
+const getTotalPriceValue =document.getElementById('totalPrice').innerText='1299';
+let totalPriceValue=parseInt(getTotalPriceValue);
+const totalPrice= extraMemoryPrice+ExtraStorageCost+DeliveryCost+totalPriceValue;
+document.getElementById('totalPrice').innerText=totalPrice;
+document.getElementById('priceWithPromoCode').innerText=totalPrice;
+
+console.log(totalPrice);
+}
+
+
+
+
+
+
+//Total Price with Promo Code
+function promoCode()
+{
+   const code =  document.getElementById('promoCode').value;
+   if(code =='stevekaku')
+   {
+     let getTotal=document.getElementById('priceWithPromoCode');
+     const getTotalPriceValue =document.getElementById('totalPrice').innerText;
+     const TotalPrice=parseInt(getTotalPriceValue);
+     const priceWithPromoCode=TotalPrice-(TotalPrice*(20/100));
+     getTotal.innerText = priceWithPromoCode;
+   }
+   else{
+      let getTotal=document.getElementById('priceWithPromoCode');
+     const getTotalPriceValue =document.getElementById('totalPrice').innerText;
+     const TotalPrice=parseInt(getTotalPriceValue);
+   //   const priceWithPromoCode=TotalPrice-(TotalPrice*(20/100));
+     getTotal.innerText = TotalPrice;
+   }
+}
