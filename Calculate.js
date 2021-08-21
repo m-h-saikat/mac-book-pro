17f1935e9e95d9f85bbc1dff423eae10473cad5d
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2021-08-21 17:05:07 +0600

    Update Function

--- Calculate.js
+++ Calculate.js
@@ -34,7 +34,9 @@ function deliveryWithCost()
    let getDeliveryCost =  document.getElementById('deliveryCharge').innerText=20;
    totalPriceCalculate();
 }
-//Total Price Calculated
+
+
+//Total Price Calculated using Function
 function totalPriceCalculate(){  
 const getExtraMemoryCost =  document.getElementById('extraMemoryCost').innerText;
 const extraMemoryPrice=parseInt(getExtraMemoryCost);
@@ -48,6 +50,19 @@ const totalPrice= extraMemoryPrice+ExtraStorageCost+DeliveryCost+totalPriceValue
 document.getElementById('totalPrice').innerText=totalPrice;
 document.getElementById('priceWithPromoCode').innerText=totalPrice;
 }
+
+
+
+//  Discount Price Calculate By Calling Function  
+
+function discountPrice (totalPrice , discountPercentage)
+{
+ const discountPricea=  totalPrice-(totalPrice*(discountPercentage/100));
+return discountPricea;
+}
+
+
+
 //Total Price with Promo Code
 function promoCode()
 {
@@ -55,9 +70,8 @@ function promoCode()
    if(code =='stevekaku')
    {
      let getTotal=document.getElementById('priceWithPromoCode');
-     const getTotalPriceValue =document.getElementById('totalPrice').innerText;
-     const TotalPrice=parseInt(getTotalPriceValue);
-     const priceWithPromoCode=TotalPrice-(TotalPrice*(20/100));
+     const TotalPrice =document.getElementById('totalPrice').innerText;
+     const priceWithPromoCode=discountPrice(TotalPrice , 20);
      getTotal.innerText = priceWithPromoCode;
      document.getElementById('promoCode').value='';
    }
@@ -65,7 +79,6 @@ function promoCode()
       let getTotal=document.getElementById('priceWithPromoCode');
      const getTotalPriceValue =document.getElementById('totalPrice').innerText;
      const TotalPrice=parseInt(getTotalPriceValue);
-   //   const priceWithPromoCode=TotalPrice-(TotalPrice*(20/100));
      getTotal.innerText = TotalPrice;
      document.getElementById('promoCode').value='';
    }
